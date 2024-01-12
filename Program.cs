d935064496bb7b8c8aae02158fd75b9ab5e5f69d
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-01-13 03:49:56 +0900

    ProgramのNullable化

--- Program.cs
+++ Program.cs
@@ -173,12 +173,12 @@ static class Program
 	/// <summary>
 	/// 実行ファイルのディレクトリ。最後に\を付けたstring
 	/// </summary>
-	public static string ExeDir { get; private set; }
-	public static string CsvDir { get; private set; }
-	public static string ErbDir { get; private set; }
-	public static string DebugDir { get; private set; }
-	public static string DatDir { get; private set; }
-	public static string ContentDir { get; private set; }
+	public static string? ExeDir { get; private set; }
+	public static string? CsvDir { get; private set; }
+	public static string? ErbDir { get; private set; }
+	public static string? DebugDir { get; private set; }
+	public static string? DatDir { get; private set; }
+	public static string? ContentDir { get; private set; }
 
 	public static bool AnalysisMode { get; private set; }
 	public static List<string> analysisFiles = [];
