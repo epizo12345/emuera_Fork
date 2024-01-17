42d8bb1084436faeb680af9587f6fcced7b36561
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-01-17 17:16:52 +0900

    例外処理の適切化

--- GameData/Variable/VariableTerm.cs
+++ GameData/Variable/VariableTerm.cs
@@ -112,7 +112,7 @@ namespace MinorShift.Emuera.GameData.Variable
 			{
 				if ((e is IndexOutOfRangeException) || (e is ArgumentOutOfRangeException) || (e is OverflowException))
 					Identifier.CheckElement(transporter);
-				throw e;
+				throw;
 			}
 		}
 
