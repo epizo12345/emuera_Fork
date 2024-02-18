6db60c3d72b7c642853a239b46648817e08bc02d
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-02-18 20:59:46 +0900

    不要な処理

--- Content/GraphicsImage.cs
+++ Content/GraphicsImage.cs
@@ -62,9 +62,6 @@ namespace MinorShift.Emuera.Content
 			Bitmap = new Bitmap(x, y, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 			size = new Size(x, y);
 			g = Graphics.FromImage(Bitmap);
-			//こうしないとbmpファイルの拡縮が綺麗に出ない
-			g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
-			g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bicubic;
 
 		}
 
@@ -77,9 +74,6 @@ namespace MinorShift.Emuera.Content
 			size = new Size(bmp.Width, bmp.Height);
 			g = Graphics.FromImage(Bitmap);
 			g.DrawImage(bmp, 0, 0, bmp.Width, bmp.Height);
-			//こうしないとbmpファイルの拡縮が綺麗に出ない
-			g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
-			g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bicubic;
 		}
 
 		/// <summary>
