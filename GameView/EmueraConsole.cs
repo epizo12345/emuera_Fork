7592ed1f042c3913deeb4644eada790f05327748
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-02-22 23:02:19 +0900

    描画タイマーが初回の描画を止めないように修正

--- GameView/EmueraConsole.cs
+++ GameView/EmueraConsole.cs
@@ -1218,7 +1218,7 @@ namespace MinorShift.Emuera.GameView
 			{
 				var sec = _stopwatch.ElapsedMilliseconds;
 				//色変化が速くなりすぎないように一定時間以内の再呼び出しは強制待ちにする
-				while (sec < 200)
+				while (lastBgColorChange != 0 && sec < 200)
 				{
 					Application.DoEvents();
 					sec = _stopwatch.ElapsedMilliseconds - lastBgColorChange;
