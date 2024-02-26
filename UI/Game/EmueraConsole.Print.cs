8de28e47bda091f1213778939a3a011b117e2251
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-02-27 08:43:33 +0900

    BAR作成を高速化

--- UI/Game/EmueraConsole.Print.cs
+++ UI/Game/EmueraConsole.Print.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -563,21 +564,42 @@ namespace MinorShift.Emuera.GameView
 
 		public string getStBar(string barStr)
 		{
-			StringBuilder bar = new();
-			bar.Append(barStr);
-			int width = 0;
-			Font font = Config.DefaultFont;
-			while (width < Config.DrawableWidth)
-			{//境界を越えるまで一文字ずつ増やす
-				bar.Append(barStr);
-				width = stringMeasure.GetDisplayLength(bar.ToString(), font);
-			}
-			while (width > Config.DrawableWidth)
-			{//境界を越えたら、今度は超えなくなるまで一文字ずつ減らす（barStrに複数字の文字列がきた場合に対応するため）
-				bar.Remove(bar.Length - 1, 1);
-				width = stringMeasure.GetDisplayLength(bar.ToString(), font);
-			}
-			return bar.ToString();
+			var bar = string.Create<string>(Config.DrawableWidth, "{0}", (span, _) =>
+			{
+				int width = 0;
+				for (int i = 0;
+				i < Config.DrawableWidth &&
+				width < Config.DrawableWidth;)
+				{
+					barStr.CopyTo(span[i..]);
+					width = stringMeasure.GetDisplayLength(span, Config.DefaultFont);
+					i += barStr.Length;
+				}
+
+				//境界を越えたら、今度は超えなくなるまで一文字ずつ減らす（barStrに複数字の文字列がきた場合に対応するため）
+				width = stringMeasure.GetDisplayLength(span, Config.DefaultFont);
+				for (int j = 1; j < Config.DrawableWidth &&
+					width > Config.DrawableWidth; j++)
+				{
+					span[^j] = '\0';
+					width = stringMeasure.GetDisplayLength(span, Config.DefaultFont);
+				}
+			});
+			// StringBuilder bar = new();
+			// bar.Append(barStr);
+			// int width = 0;
+			// Font font = Config.DefaultFont;
+			// while (width < Config.DrawableWidth)
+			// {//境界を越えるまで一文字ずつ増やす
+			// 	bar.Append(barStr);
+			// 	width = stringMeasure.GetDisplayLength(bar.ToString(), font);
+			// }
+			// while (width > Config.DrawableWidth)
+			// {//境界を越えたら、今度は超えなくなるまで一文字ずつ減らす（barStrに複数字の文字列がきた場合に対応するため）
+			// 	bar.Remove(bar.Length - 1, 1);
+			// 	width = stringMeasure.GetDisplayLength(bar.ToString(), font);
+			// }
+			return bar;
 		}
 
 		public void setStBar(string barStr)
