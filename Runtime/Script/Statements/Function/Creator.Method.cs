3fae5112de9b608f94842b1950b12c742bd31288
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-02-27 13:01:07 +0900

    GetMilliSeccendが西暦0001年1月1日からの経過時間を返すように修正

--- Runtime/Script/Statements/Function/Creator.Method.cs
+++ Runtime/Script/Statements/Function/Creator.Method.cs
@@ -893,8 +893,7 @@ namespace MinorShift.Emuera.GameData.Function
             public override Int64 GetIntValue(ExpressionMediator exm, AExpression[] arguments)
             {
                 //西暦0001年1月1日からの経過時間をミリ秒で。
-                //Ticksは100ナノ秒単位であるが実際にはそんな精度はないので無駄。
-                return DateTime.Now.Ticks / 10000;
+                return (long)(DateTime.Now - new DateTime(0001, 1, 1, 0, 0, 0)).TotalMilliseconds;
             }
         }
 
