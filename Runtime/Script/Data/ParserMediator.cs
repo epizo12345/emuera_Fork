4269046ecc865e6d5858bd3f17d1cb23a9b4bc4e
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-04-15 21:35:49 +0900

    エラー追加処理の微修正

--- Runtime/Script/Data/ParserMediator.cs
+++ Runtime/Script/Data/ParserMediator.cs
@@ -78,6 +78,7 @@ internal static partial class ParserMediator
     }
     #endregion
 
+    static object warningListLock = new();
 
     public static void Warn(string str, ScriptPosition? pos, int level)
     {
@@ -89,7 +90,12 @@ internal static partial class ParserMediator
         if (level < Config.DisplayWarningLevel && !Program.AnalysisMode)
             return;
         if (console != null && !console.RunERBFromMemory)
-            warningList.Add(new ParserWarning(str, pos, level, stack));
+        {
+            lock (warningListLock)
+            {
+                warningList.Add(new ParserWarning(str, pos, level, stack));
+            }
+        }
     }
 
     /// <summary>
