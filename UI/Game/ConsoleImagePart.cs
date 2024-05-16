12adc18ff9fa2544ac05cdda8d5d5ca15b074825
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-05-16 19:27:34 +0900

    エラーテキストを黒で表示してしまう問題を修正

--- UI/Game/ConsoleImagePart.cs
+++ UI/Game/ConsoleImagePart.cs
@@ -208,7 +208,7 @@ sealed class ConsoleImagePart : AConsoleDisplayNode
         }
         else
         {
-            graph.DrawText(AltText, PointX, point.Y, SKTextAlign.Left, new SKFont(), new SKPaint());
+            graph.DrawText(AltText, PointX, point.Y, SKTextAlign.Left, new SKFont(), new SKPaint() { Color = Config.ForeColor.ToSKColor() });
         }
     }
 }
