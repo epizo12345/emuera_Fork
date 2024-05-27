9e0c42bd85fc891294639cb3c0b66ae4878ff4fa
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-05-27 13:14:41 +0900

    デフォルトのリサンプラーをLinearに指定

--- Runtime/Config/JSON/JSONConfigData.cs
+++ Runtime/Config/JSON/JSONConfigData.cs
@@ -20,7 +20,7 @@ sealed class JSONConfigData
 
     public bool UseScopedVariableInstruction { get; set; }
 
-    public Resampler ImageSamplingOption { get; set; }
+    public Resampler ImageSamplingOption { get; set; } = Resampler.Linear;
 
     public bool CheckUTF8withBOM { get; set; } = true;
 }
