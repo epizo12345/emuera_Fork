67cd3a4dab035409a0b0ffa139f517d00dafb919
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-05-29 12:45:36 +0900

    描画の数値調整

--- Runtime/Config/Config.cs
+++ Runtime/Config/Config.cs
@@ -167,7 +167,7 @@ internal static class Config
             TextDrawingMode = TextDrawingMode.TEXTRENDERER;
         }
 
-        DrawingParam_ShapePositionShift = 2;
+        DrawingParam_ShapePositionShift = 3;
         DrawableWidth = WindowX - DrawingParam_ShapePositionShift;
         ForceSavDir = Program.ExeDir + "sav" + Path.DirectorySeparatorChar;
         if (UseSaveFolder)
