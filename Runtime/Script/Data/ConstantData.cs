b4074d189cfce071571d6ee69a085acb589a1240
Vivilet <52270516+Vivilet@users.noreply.github.com>
2024-06-30 13:39:02 +0900

    CALLNAMEがハンドリングできないバグを修正

--- Runtime/Script/Data/ConstantData.cs
+++ Runtime/Script/Data/ConstantData.cs
@@ -873,6 +873,7 @@ internal sealed class ConstantData
                 allowIndex = 1;
                 break;
             case VariableCode.NAME:
+            case VariableCode.CALLNAME:
                 ret = relationDic;
                 errPos = "chara*.csv";
                 allowIndex = -1;
