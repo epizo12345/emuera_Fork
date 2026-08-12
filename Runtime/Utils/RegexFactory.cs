63f8ed32396abe658dd1046e4ffec211de757c4e
epizo <eryushion@gmail.com>
2026-08-12 15:15:02 +0900

    refactor: RegexFactoryのキャッシュ取得処理を単純化

--- Runtime/Utils/RegexFactory.cs
+++ Runtime/Utils/RegexFactory.cs
@@ -9,25 +9,11 @@ static class RegexFactory
 
     public static Regex GetRegex(string regex)
     {
-
         if (_dictionary.TryGetValue(regex, out var ret))
-        {
             return ret;
-        }
-        else
-        {
-            try
-            {
-                ret = new Regex(regex, RegexOptions.Compiled);
-                _dictionary.Add(regex, ret);
-            }
-            catch (ArgumentException)
-            {
-                throw;
-            }
-        }
 
+        ret = new Regex(regex, RegexOptions.Compiled);
+        _dictionary.Add(regex, ret);
         return ret;
-
     }
 }
